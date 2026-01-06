9ac6d513fba6c8b4cf484e07379fbe040142c2e8
Iceyami <137309137+iceYami@users.noreply.github.com>
2026-01-06 19:14:51 +0100

    Create stuxnet_techies.py

---
+++ stuxnet_techies.py
@@ -0,0 +1,37 @@
+{
+  "name": "Stuxnet ATT&CK Layer",
+  "versions": {
+    "attack": "13",
+    "navigator": "4.9.0",
+    "layer": "4.4"
+  },
+  "domain": "ics-attack",
+  "description": "Techniques used in Stuxnet operation",
+  "techniques": [
+    {
+      "techniqueID": "T0883",
+      "color": "#ff0000",
+      "comment": "USB worm propagation"
+    },
+    {
+      "techniqueID": "T0873",
+      "color": "#ff0000",
+      "comment": "Step 7 project infection"
+    },
+    {
+      "techniqueID": "T0839",
+      "color": "#ff0000",
+      "comment": "PLC firmware rootkit"
+    },
+    {
+      "techniqueID": "T0836",
+      "color": "#ff0000",
+      "comment": "Centrifuge frequency manipulation"
+    },
+    {
+      "techniqueID": "T0879",
+      "color": "#ff0000",
+      "comment": "Physical destruction of centrifuges"
+    }
+  ]
+}
