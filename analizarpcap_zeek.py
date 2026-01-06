49abccd4fec68e0a89367b39cda66dd5c32c87de
Iceyami <137309137+iceYami@users.noreply.github.com>
2026-01-06 19:05:28 +0100

    Create analizarpcap_zeek.py

---
+++ analizarpcap_zeek.py
@@ -0,0 +1,6 @@
+zeek -r capture.pcap /opt/zeek/share/zeek/policy/protocols/modbus
+
+# Output files:
+# - modbus.log: Modbus transactions
+# - conn.log: All connections
+# - weird.log: Protocol anomalies
