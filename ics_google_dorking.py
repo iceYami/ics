07de766a86c0fd46f52bbe0b7e2bf8b00ad4e153
Iceyami <137309137+iceYami@users.noreply.github.com>
2026-01-06 19:09:01 +0100

    Create ics_google_dorking.py

---
+++ ics_google_dorking.py
@@ -0,0 +1,25 @@
+# SCADA HMI web interfaces
+inurl:scada
+inurl:hmi
+intitle:"SCADA Login"
+intitle:"Wonderware InTouch"
+intitle:"WinCC"
+
+# Engineering portals
+intitle:"TIA Portal"
+inurl:"/portal/login"
+
+# Historians
+intitle:"PI Vision"
+intitle:"OSIsoft"
+
+# Building automation
+intitle:"Niagara" inurl:"/config"
+intitle:"BACnet" inurl:"/admin"
+
+# IP cameras (often on OT networks)
+inurl:/view/index.shtml
+intitle:"Network Camera"
+
+# Manufacturer defaults
+intitle:"admin" inurl:login.asp "Siemens"
