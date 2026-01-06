0bab9fca52f8fb0d71e8d767027a682915696977
Iceyami <137309137+iceYami@users.noreply.github.com>
2026-01-06 22:22:02 +0100

    Create EXIF.sh

---
+++ EXIF.sh
@@ -0,0 +1,12 @@
+# Install exiftool
+sudo apt install libimage-exiftool-perl
+
+# Extract metadata
+exiftool network_diagram.jpg
+
+# Look for:
+# - GPS coordinates (facility location)
+# - Camera/software info
+# - Author (employee name)
+# - Creation date
+# - Company name in metadata
