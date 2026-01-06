673d71902494bbae6a36a41a6f53042cc876e4f2
Iceyami <137309137+iceYami@users.noreply.github.com>
2026-01-06 19:09:50 +0100

    Create busqueda_creds_expuestos.py

---
+++ busqueda_creds_expuestos.py
@@ -0,0 +1,14 @@
+# HMI configuration files
+extension:scada
+extension:db1
+extension:s7p
+
+# Credentials in scripts
+"modbus" AND "password" filename:.py
+"s7comm" AND "192.168" filename:.py
+
+# Network diagrams
+"PLC" filetype:vsd
+"SCADA" filetype:pdf
+
+# Use github-search or grep.app
